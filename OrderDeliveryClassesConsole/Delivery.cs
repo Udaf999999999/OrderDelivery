10bb6e34b16418dc42c00d842559d2b8ac1c35cb
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-12 16:17:14 +0700

    Add Delivery.cs and make abstract class Delivery

---
+++ OrderDeliveryClassesConsole/Delivery.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDeliveryClassesConsole
+{
+    internal abstract class Delivery
+    {
+        public string Address;
+    }
+    class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class Order<TDelivery,
+    TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        // ... Другие поля
+    }
+}
