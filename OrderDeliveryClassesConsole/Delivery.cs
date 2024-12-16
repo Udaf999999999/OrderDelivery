95d565c6f1375018154af3d63652547d2600846d
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-16 16:03:38 +0700

    Add same classes

--- OrderDeliveryClassesConsole/Delivery.cs
+++ OrderDeliveryClassesConsole/Delivery.cs
@@ -6,18 +6,60 @@ using System.Threading.Tasks;
 
 namespace OrderDeliveryClassesConsole
 {
-    
-    internal abstract class Delivery
+    public enum Status
+    {
+        Complite,
+        Underway,
+        Cancelled
+    }
+
+    public abstract class Delivery
     {
         public string Address;
+        protected DateTime deliveryDate;
+        public DateTime DeliveryDate
+        {
+            get { return deliveryDate; }
+        }
+        public Status Status { get; set; }
+        public void UpdateStatus(Status newStatus)
+        {
+            Status = newStatus;
+        }
+        public virtual void UpdateDeliveryDate()
+        {
+
+        }
     }
     /// <summary>
     /// доставка на дом. Этот тип будет подразумевать наличие курьера или передачу курьерской компании, 
     /// в нем будет располагаться своя, отдельная от прочих типов доставки логика.
     /// </summary>
-    class HomeDelivery : Delivery
+    public abstract class HomeDelivery : Delivery
     {
+
+        //public HomeDelivery()
+        //{
+
+
+        //}
         /* ... */
+
+    }
+    public class OurHomeDelivery : HomeDelivery
+    {
+        private DateTime CalculationTimeDelivery()//Реализовать данный метод
+        {
+            return DateTime.Now;
+        }
+        public override void UpdateDeliveryDate()
+        {
+            deliveryDate = CalculationTimeDelivery();
+        }
+    }
+    public class Incity : OurHomeDelivery
+    {
+
     }
     /// <summary>
     /// доставка в пункт выдачи. Здесь будет храниться какая-то ещё логика, 
@@ -38,17 +80,89 @@ namespace OrderDeliveryClassesConsole
         /* ... */
     }
 
-    class Order<TDelivery,TStruct> where TDelivery : Delivery
+    public class Product
+    {
+        private int id;
+        public string Title;
+        public string Discripsion;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (value >= 0)
+                {
+                    id = value;
+                }
+                else
+                {
+                    Console.WriteLine("Id can't be under zero!");
+                }
+            }
+        }
+
+        public Product(int id, string title, string discripsion)
+        {
+            this.id = id;
+            Title = title;
+            Discripsion = discripsion;
+        }
+        public Product(int id, string title)
+        {
+            this.id = id;
+            Title = title;
+            Discripsion = null;
+        }
+    }
+    
+
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
     {
         public TDelivery Delivery;
+        public class ProductCount
+        {
+            public Product product;
+            public int count;
+            public ProductCount(Product product, int count)
+            {
+                this.product = product;
+                this.count = count;
+            }
+        }
 
-        public int Number;
+        private ProductCount[] productCount;
+        public static int maxProductCount = 10;
 
-        public string Description;
+        public int id;
+        public string CustumerName;
+
+        public Order(string[] order)
+        {
+            if (order.Length > maxProductCount)
+            {
+                Console.WriteLine("Too many orders!");
+            }
+            else
+            {
+                productCount = new ProductCount[order.Length];
+                for (int i = 0; i < order.Length; i++)
+                {
+                    //реализовать парсинг,
+
+                }
+            }
+        }
 
         public void DisplayAddress()
         {
             Console.WriteLine(Delivery.Address);
+        }
+
+        public (int, Product) this[int index]//Переделать на картеж
+        {
+            get { return (productCount[index].count, productCount[index].product);   }
+            set 
+            { productCount[index] = new ProductCount(value.Item2,value.Item1); }
         }
 
         // ... Другие поля
